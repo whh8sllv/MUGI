2dde79ff8e5723ff429c4690ab3991461fb8c7d8
Anastasiia Kovalchuk <anastasiia.kovalchuk@factum-ua.com>
2025-05-24 16:34:22 +0300

    Class MUGI

--- main.py
+++ main.py
@@ -1,213 +1,192 @@
-def left_shift(bits, n):
-    return ((bits << n) | (bits >> (64 - n))) & (2**64 - 1)
+class MUGI():
 
-def right_shift(bits, n):
-    return ((bits >> n) | (bits << (64 - n))) & (2**64 - 1)
+    def __init__(self, key, IV):
+        self.key = key
+        self.IV = IV
+        self.D0 = 0x6a09e667f3bcc908
+        self.D1 = 0xbb67ae8584caa73b
+        self.D2 = 0x3c6ef372fe94f82b
 
-def init(key, IV):
-    D0 = 0x6a09e667f3bcc908
-    key_0 = int(('0x' + key[:16]), 16)
-    key_1 = int(('0x' + key[16:]), 16)
-    a_49_2 = (left_shift(key_0, 7)) ^ (right_shift(key_1, 7)) ^ D0
-    a_49 = [key_0, key_1, a_49_2]
-    b_16 = []
-    temp = a_49
-    for i in range(16):
-        a_new = function_rho1(temp, 0, 0)
-        temp = a_new
-        b_16.append(a_new[0])
-    a_33 = a_new
-    b_16 =  b_16[::-1]
-    IV_0 = int(('0x' + IV[:16]), 16)
-    IV_1 = int(('0x' + IV[16:]), 16)
-    a_32_0 = a_33[0] ^ IV_0
-    a_32_1 = a_33[1] ^ IV_1
-    a_32_2 = a_33[2] ^ (left_shift(IV_0, 7)) ^ (right_shift(IV_1, 7)) ^ D0
-    a_32 = [a_32_0, a_32_1, a_32_2]
-    temp = a_32
-    for i in range(16):
-        a_new = function_rho1(temp, 0, 0)
-        temp = a_new
-    a_16 = a_new
-    temp_a, temp_b = a_16, b_16
-    z_i = ''
-    for i in range(16):
-        a_new = next(temp_a, temp_b)[0]
-        b_new = next(temp_a, temp_b)[1]
-        z = next(temp_a, temp_b)[2]
-        temp_a, temp_b = a_new, b_new
-    return temp_a, temp_b, z
+    def initCipher(self):
+        key_0 = int(('0x' + self.key[:16]), 16)
+        key_1 = int(('0x' + self.key[16:]), 16)
+        a_49_2 = (self.left_shift(key_0, 7)) ^ (self.right_shift(key_1, 7)) ^ self.D0
+        a_49 = [key_0, key_1, a_49_2]
+        b_16 = []
+        temp = a_49
+        for i in range(16):
+            a_new = self.function_rho1(temp, 0, 0)
+            temp = a_new
+            b_16.append(a_new[0])
+        a_33 = a_new
+        b_16 =  b_16[::-1]
+        IV_0 = int(('0x' + self.IV[:16]), 16)
+        IV_1 = int(('0x' + self.IV[16:]), 16)
+        a_32_0 = a_33[0] ^ IV_0
+        a_32_1 = a_33[1] ^ IV_1
+        a_32_2 = a_33[2] ^ (self.left_shift(IV_0, 7)) ^ (self.right_shift(IV_1, 7)) ^ self.D0
+        a_32 = [a_32_0, a_32_1, a_32_2]
+        temp = a_32
+        for i in range(16):
+            a_new = self.function_rho1(temp, 0, 0)
+            temp = a_new
+        a_16 = a_new
+        temp_a, temp_b = a_16, b_16
+        z_i = ''
+        for i in range(16):
+            a_new = self.next_and_stream(temp_a, temp_b)[0]
+            b_new = self.next_and_stream(temp_a, temp_b)[1]
+            z = self.next_and_stream(temp_a, temp_b)[2]
+            temp_a, temp_b = a_new, b_new
+        return temp_a, temp_b, z
 
-def next(a, b):
-    a_next = function_rho1(a, b[4], b[10])
-    b_next = function_lambda1(b, a[0])
-    z_i = hex(a_next[2])[2:]
-    if len(z_i) < 16:
-        dif = 16 - len(z_i)
-        z_i = '0' * dif + z_i
-    return a_next, b_next, z_i
-
-def make_sequence(a_0, b_0, z, text_length_byte):
-    temp_a = a_0
-    temp_b = b_0
-    while len(bytes.fromhex(z)) < text_length_byte:
-        a_new = next(temp_a, temp_b)[0]
-        b_new = next(temp_a, temp_b)[1]
-        z += next(temp_a, temp_b)[2]
-        temp_a, temp_b = a_new, b_new
-    return z
-
-def function_lambda1(b, a_0):
-    b_new = [0] * 16
-    for j in range(16):
-        if j == 0 or j == 4 or j == 10:
-            continue
-        b_new[j] = b[j-1]
-    b_new[0] = b[15] ^ a_0
-    b_new[4] = b[3] ^ b[7]
-    b_new[10] = b[9] ^ left_shift(b[13], 32)
-    return b_new
-
-def function_rho1(a_i, w1, w2):
-    D1 = 0xbb67ae8584caa73b
-    D2 = 0x3c6ef372fe94f82b
-    a_0_new = a_i[1]
-    a_1_new = a_i[2] ^ function_F(a_i[1], w1) ^ D1
-    a_2_new = a_i[0] ^ function_F(a_i[1], left_shift(w2, 17)) ^ D2
-    return [a_0_new, a_1_new, a_2_new]
-
-def function_F(x, t):
-    x_new = hex(x ^ t)[2:]
-    if len(x_new) < 16:
-        dif = 16 - len(x_new)
-        x_new = '0' * dif + x_new
-    x_i = []
-    for i in range(1, len(x_new), 2):
-        x_i.append(x_new[i-1] + x_new[i])
-    p_i = [function_S_R(i)[2:] for i in x_i]
-    q_l = function_M(p_i[0], p_i[1], p_i[2], p_i[3])
-    q_r = function_M(p_i[4], p_i[5], p_i[6], p_i[7])
-    return int(('0x' + q_r[0] + q_r[1] + q_l[2] + q_l[3] + q_l[0] + q_l[1] + q_r[2] + q_r[3]), 16)
-
-def function_S_R(x):
-    x = int('0x' + x, 16)
-    SUB = ['0x63', '0x7c', '0x77', '0x7b', '0xf2', '0x6b', '0x6f', '0xc5', '0x30', '0x01', '0x67', '0x2b', '0xfe', '0xd7', '0xab', '0x76', 
-           '0xca', '0x82', '0xc9', '0x7d', '0xfa', '0x59', '0x47', '0xf0', '0xad', '0xd4', '0xa2', '0xaf', '0x9c', '0xa4', '0x72', '0xc0',
-           '0xb7', '0xfd', '0x93', '0x26', '0x36', '0x3f', '0xf7', '0xcc', '0x34', '0xa5', '0xe5', '0xf1', '0x71', '0xd8', '0x31', '0x15',
-           '0x04', '0xc7', '0x23', '0xc3', '0x18', '0x96', '0x05', '0x9a', '0x07', '0x12', '0x80', '0xe2', '0xeb', '0x27', '0xb2', '0x75',
-           '0x09', '0x83', '0x2c', '0x1a', '0x1b', '0x6e', '0x5a', '0xa0', '0x52', '0x3b', '0xd6', '0xb3', '0x29', '0xe3', '0x2f', '0x84',
-           '0x53', '0xd1', '0x00', '0xed', '0x20', '0xfc', '0xb1', '0x5b', '0x6a', '0xcb', '0xbe', '0x39', '0x4a', '0x4c', '0x58', '0xcf',
-           '0xd0', '0xef', '0xaa', '0xfb', '0x43', '0x4d', '0x33', '0x85', '0x45', '0xf9', '0x02', '0x7f', '0x50', '0x3c', '0x9f', '0xa8',
-           '0x51', '0xa3', '0x40', '0x8f', '0x92', '0x9d', '0x38', '0xf5', '0xbc', '0xb6', '0xda', '0x21', '0x10', '0xff', '0xf3', '0xd2',
-           '0xcd', '0x0c', '0x13', '0xec', '0x5f', '0x97', '0x44', '0x17', '0xc4', '0xa7', '0x7e', '0x3d', '0x64', '0x5d', '0x19', '0x73',
-           '0x60', '0x81', '0x4f', '0xdc', '0x22', '0x2a', '0x90', '0x88', '0x46', '0xee', '0xb8', '0x14', '0xde', '0x5e', '0x0b', '0xdb',
-           '0xe0', '0x32', '0x3a', '0x0a', '0x49', '0x06', '0x24', '0x5c', '0xc2', '0xd3', '0xac', '0x62', '0x91', '0x95', '0xe4', '0x79',
-           '0xe7', '0xc8', '0x37', '0x6d', '0x8d', '0xd5', '0x4e', '0xa9', '0x6c', '0x56', '0xf4', '0xea', '0x65', '0x7a', '0xae', '0x08',
-           '0xba', '0x78', '0x25', '0x2e', '0x1c', '0xa6', '0xb4', '0xc6', '0xe8', '0xdd', '0x74', '0x1f', '0x4b', '0xbd', '0x8b', '0x8a',
-           '0x70', '0x3e', '0xb5', '0x66', '0x48', '0x03', '0xf6', '0x0e', '0x61', '0x35', '0x57', '0xb9', '0x86', '0xc1', '0x1d', '0x9e',
-           '0xe1', '0xf8', '0x98', '0x11', '0x69', '0xd9', '0x8e', '0x94', '0x9b', '0x1e', '0x87', '0xe9', '0xce', '0x55', '0x28', '0xdf',
-           '0x8c', '0xa1', '0x89', '0x0d', '0xbf', '0xe6', '0x42', '0x68', '0x41', '0x99', '0x2d', '0x0f', '0xb0', '0x54', '0xbb', '0x16']
-    return SUB[x]
-
-def function_M(x0, x1, x2, x3):
-    m = 8
-    f_x = get_int('100011011')
-    const1 = get_int(make_polynomials(bin(1)[2:]))
-    const2 = get_int(make_polynomials(bin(2)[2:]))
-    const3 = get_int(make_polynomials(bin(3)[2:]))
-    x0 = get_int(make_polynomials(bin(int('0x' + x0, 16))[2:]))
-    x1 = get_int(make_polynomials(bin(int('0x' + x1, 16))[2:]))
-    x2 = get_int(make_polynomials(bin(int('0x' + x2, 16))[2:]))
-    x3 = get_int(make_polynomials(bin(int('0x' + x3, 16))[2:]))
-    y0 = hex(polynomials_multiply(const2, x0, f_x) ^ polynomials_multiply(const3, x1, f_x) ^ polynomials_multiply(const1, x2, f_x) ^ polynomials_multiply(const1, x3, f_x))[2:]
-    y1 = hex(polynomials_multiply(const1, x0, f_x) ^ polynomials_multiply(const2, x1, f_x) ^ polynomials_multiply(const3, x2, f_x) ^ polynomials_multiply(const1, x3, f_x))[2:]
-    y2 = hex(polynomials_multiply(const1, x0, f_x) ^ polynomials_multiply(const1, x1, f_x) ^ polynomials_multiply(const2, x2, f_x) ^ polynomials_multiply(const3, x3, f_x))[2:]
-    y3 = hex(polynomials_multiply(const3, x0, f_x) ^ polynomials_multiply(const1, x1, f_x) ^ polynomials_multiply(const1, x2, f_x) ^ polynomials_multiply(const2, x3, f_x))[2:]
-    sup = [y0, y1, y2, y3]
-    res = []
-    for y_i in sup:
-        if len(y_i) < 2:
-            res.append('0' + y_i)
-        else:
-            res.append(y_i)
-    print            
-    return res
-
-def get_int(string):
-    return [int(i) for i in string]
-
-def make_polynomials(num):
-    while len(num) < 8:
-        num = '0' + num
-    return num
-
-def polynomials_module(a, f_x):
-    if len(a) < len(f_x):
-        return a
-    for i in range(len(f_x)):
-        a[i] = (a[i] + f_x[i]) % 2
-    while a and a[0] == 0:
-        a.pop(0)
-    return polynomials_module(a, f_x)
-
-def polynomials_multiply(a, b, f_x):
-    len_res = len(a) + len(b) - 1
-    res = [0] * len_res
-    for i in range(len(a)):
-        if a[i] == 1:
-            for j in range(len(b)):
-                res[i+j] = (res[i+j] + b[j]) % 2
-    module = ''.join(str(i) for i in polynomials_module(res, f_x))
-    if module == '':
-        module += '0'
-    return int(('0b' + module), 2)
+    def next_and_stream(self, a, b):
+        a_next = self.function_rho1(a, b[4], b[10])
+        b_next = self.function_lambda1(b, a[0])
+        z_i = hex(a_next[2])[2:]
+        if len(z_i) < 16:
+            dif = 16 - len(z_i)
+            z_i = '0' * dif + z_i
+        return a_next, b_next, z_i
     
+    def make_keystream(self, a_0, b_0, z, text_length_byte):
+        temp_a = a_0
+        temp_b = b_0
+        while len(bytes.fromhex(z)) < text_length_byte:
+            a_new = self.next_and_stream(temp_a, temp_b)[0]
+            b_new = self.next_and_stream(temp_a, temp_b)[1]
+            z += self.next_and_stream(temp_a, temp_b)[2]
+            temp_a, temp_b = a_new, b_new
+        return z
+    
+    def function_lambda1(self, b, a_0):
+        b_new = [0] * 16
+        for j in range(16):
+            if j == 0 or j == 4 or j == 10:
+                continue
+            b_new[j] = b[j-1]
+        b_new[0] = b[15] ^ a_0
+        b_new[4] = b[3] ^ b[7]
+        b_new[10] = b[9] ^ self.left_shift(b[13], 32)
+        return b_new
+    
+    def function_rho1(self, a_i, w1, w2):
+        a_0_new = a_i[1]
+        a_1_new = a_i[2] ^ self.function_F(a_i[1], w1) ^ self.D1
+        a_2_new = a_i[0] ^ self.function_F(a_i[1], self.left_shift(w2, 17)) ^ self.D2
+        return [a_0_new, a_1_new, a_2_new]
+    
+    def function_F(self, x, t):
+        x_new = hex(x ^ t)[2:]
+        if len(x_new) < 16:
+            dif = 16 - len(x_new)
+            x_new = '0' * dif + x_new
+        x_i = []
+        for i in range(1, len(x_new), 2):
+            x_i.append(x_new[i-1] + x_new[i])
+        p_i = [self.function_S_R(i)[2:] for i in x_i]
+        q_l = self.function_M(p_i[0], p_i[1], p_i[2], p_i[3])
+        q_r = self.function_M(p_i[4], p_i[5], p_i[6], p_i[7])
+        return int(('0x' + q_r[0] + q_r[1] + q_l[2] + q_l[3] + q_l[0] + q_l[1] + q_r[2] + q_r[3]), 16)
+    
+    def function_S_R(self, x):
+        x = int('0x' + x, 16)
+        SUB = ['0x63', '0x7c', '0x77', '0x7b', '0xf2', '0x6b', '0x6f', '0xc5', '0x30', '0x01', '0x67', '0x2b', '0xfe', '0xd7', '0xab', '0x76', 
+               '0xca', '0x82', '0xc9', '0x7d', '0xfa', '0x59', '0x47', '0xf0', '0xad', '0xd4', '0xa2', '0xaf', '0x9c', '0xa4', '0x72', '0xc0',
+               '0xb7', '0xfd', '0x93', '0x26', '0x36', '0x3f', '0xf7', '0xcc', '0x34', '0xa5', '0xe5', '0xf1', '0x71', '0xd8', '0x31', '0x15',
+               '0x04', '0xc7', '0x23', '0xc3', '0x18', '0x96', '0x05', '0x9a', '0x07', '0x12', '0x80', '0xe2', '0xeb', '0x27', '0xb2', '0x75',
+               '0x09', '0x83', '0x2c', '0x1a', '0x1b', '0x6e', '0x5a', '0xa0', '0x52', '0x3b', '0xd6', '0xb3', '0x29', '0xe3', '0x2f', '0x84',
+               '0x53', '0xd1', '0x00', '0xed', '0x20', '0xfc', '0xb1', '0x5b', '0x6a', '0xcb', '0xbe', '0x39', '0x4a', '0x4c', '0x58', '0xcf',
+               '0xd0', '0xef', '0xaa', '0xfb', '0x43', '0x4d', '0x33', '0x85', '0x45', '0xf9', '0x02', '0x7f', '0x50', '0x3c', '0x9f', '0xa8',
+               '0x51', '0xa3', '0x40', '0x8f', '0x92', '0x9d', '0x38', '0xf5', '0xbc', '0xb6', '0xda', '0x21', '0x10', '0xff', '0xf3', '0xd2',
+               '0xcd', '0x0c', '0x13', '0xec', '0x5f', '0x97', '0x44', '0x17', '0xc4', '0xa7', '0x7e', '0x3d', '0x64', '0x5d', '0x19', '0x73',
+               '0x60', '0x81', '0x4f', '0xdc', '0x22', '0x2a', '0x90', '0x88', '0x46', '0xee', '0xb8', '0x14', '0xde', '0x5e', '0x0b', '0xdb',
+               '0xe0', '0x32', '0x3a', '0x0a', '0x49', '0x06', '0x24', '0x5c', '0xc2', '0xd3', '0xac', '0x62', '0x91', '0x95', '0xe4', '0x79',
+               '0xe7', '0xc8', '0x37', '0x6d', '0x8d', '0xd5', '0x4e', '0xa9', '0x6c', '0x56', '0xf4', '0xea', '0x65', '0x7a', '0xae', '0x08',
+               '0xba', '0x78', '0x25', '0x2e', '0x1c', '0xa6', '0xb4', '0xc6', '0xe8', '0xdd', '0x74', '0x1f', '0x4b', '0xbd', '0x8b', '0x8a',
+               '0x70', '0x3e', '0xb5', '0x66', '0x48', '0x03', '0xf6', '0x0e', '0x61', '0x35', '0x57', '0xb9', '0x86', '0xc1', '0x1d', '0x9e',
+               '0xe1', '0xf8', '0x98', '0x11', '0x69', '0xd9', '0x8e', '0x94', '0x9b', '0x1e', '0x87', '0xe9', '0xce', '0x55', '0x28', '0xdf',
+               '0x8c', '0xa1', '0x89', '0x0d', '0xbf', '0xe6', '0x42', '0x68', '0x41', '0x99', '0x2d', '0x0f', '0xb0', '0x54', '0xbb', '0x16']
+        return SUB[x]
+    
+    def function_M(self, x0, x1, x2, x3):
+        m = 8
+        f_x = self.get_int('100011011')
+        const1 = self.get_int(self.make_polynomials(bin(1)[2:]))
+        const2 = self.get_int(self.make_polynomials(bin(2)[2:]))
+        const3 = self.get_int(self.make_polynomials(bin(3)[2:]))
+        x0 = self.get_int(self.make_polynomials(bin(int('0x' + x0, 16))[2:]))
+        x1 = self.get_int(self.make_polynomials(bin(int('0x' + x1, 16))[2:]))
+        x2 = self.get_int(self.make_polynomials(bin(int('0x' + x2, 16))[2:]))
+        x3 = self.get_int(self.make_polynomials(bin(int('0x' + x3, 16))[2:]))
+        y0 = hex(self.polynomials_multiply(const2, x0, f_x) ^ self.polynomials_multiply(const3, x1, f_x) ^ self.polynomials_multiply(const1, x2, f_x) ^ self.polynomials_multiply(const1, x3, f_x))[2:]
+        y1 = hex(self.polynomials_multiply(const1, x0, f_x) ^ self.polynomials_multiply(const2, x1, f_x) ^ self.polynomials_multiply(const3, x2, f_x) ^ self.polynomials_multiply(const1, x3, f_x))[2:]
+        y2 = hex(self.polynomials_multiply(const1, x0, f_x) ^ self.polynomials_multiply(const1, x1, f_x) ^ self.polynomials_multiply(const2, x2, f_x) ^ self.polynomials_multiply(const3, x3, f_x))[2:]
+        y3 = hex(self.polynomials_multiply(const3, x0, f_x) ^ self.polynomials_multiply(const1, x1, f_x) ^ self.polynomials_multiply(const1, x2, f_x) ^ self.polynomials_multiply(const2, x3, f_x))[2:]
+        sup = [y0, y1, y2, y3]
+        res = []
+        for y_i in sup:
+            if len(y_i) < 2:
+                res.append('0' + y_i)
+            else:
+                res.append(y_i)          
+        return res
+    
+    def get_int(self, string):
+        return [int(i) for i in string]
+    
+    def make_polynomials(self, num):
+        while len(num) < 8:
+            num = '0' + num
+        return num
+    
+    def polynomials_module(self, a, f_x):
+        if len(a) < len(f_x):
+            return a
+        for i in range(len(f_x)):
+            a[i] = (a[i] + f_x[i]) % 2
+        while a and a[0] == 0:
+            a.pop(0)
+        return self.polynomials_module(a, f_x)
+    
+    def polynomials_multiply(self, a, b, f_x):
+        len_res = len(a) + len(b) - 1
+        res = [0] * len_res
+        for i in range(len(a)):
+            if a[i] == 1:
+                for j in range(len(b)):
+                    res[i+j] = (res[i+j] + b[j]) % 2
+        module = ''.join(str(i) for i in self.polynomials_module(res, f_x))
+        if module == '':
+            module += '0'
+        return int(('0b' + module), 2)
 
-def encrypt_text(plaintext, key, vector):
-    with open(plaintext, 'rb') as text_bytes:
-        input_bytes = text_bytes.read()
-    byte_length = len(input_bytes)
-    s_0 = init(key, vector)
-    z_res = make_sequence(s_0[0], s_0[1], s_0[2], byte_length)
-    z = bytes.fromhex(z_res)
-    ciphertext = ''
-    for i in range(byte_length):
-        xor = hex(input_bytes[i] ^ z[i])[2:]
-        if len(xor) < 2:
-            xor = '0' + xor
-        ciphertext += xor
-    return ciphertext, z_res
-
-def decrypt_text(ciphertext, z):
-    z_i = bytes.fromhex(z)
-    ciphertext_i = bytes.fromhex(ciphertext)
-    res = []
-    for i in range(len(ciphertext_i)):
-        res.append(z_i[i] ^ ciphertext_i[i])
-    res_bytes = bytes(res)
-    return res_bytes.decode('utf-8')
-
-
-
-cipher = encrypt_text('test1.txt', '000102030405060708090a0b0c0d0e0f', 'f0e0d0c0b0a090807060504030201000')[0]
-z_i = encrypt_text('test1.txt', '000102030405060708090a0b0c0d0e0f', 'f0e0d0c0b0a090807060504030201000')[1]
-
-print(z_i)
-
-print(decrypt_text(cipher, z_i))
-
-
-
-key = '000102030405060708090a0b0c0d0e0f'
-key1 ='00000000000000000000000000000000'
-vector = 'f0e0d0c0b0a090807060504030201000'
-vector1 = '00000000000000000000000000000000'
-
-key2 = '00000000000000000000000000000000'
-vector2 = '3461698851812139015500a53b7e5987'
-
-key3 = '513400b104a0599130ad00fc48d759e0'
-vector3 = '00000000000000000000000000000000'
-
-key4 = '69e706ee5295372c7513014730237993'
-vector4 = '2a0045c8492749d53a9b164a25e44915'
+    def left_shift(self, bits, n):
+        return ((bits << n) | (bits >> (64 - n))) & (2**64 - 1) 
+    
+    def right_shift(self, bits, n):
+        return ((bits >> n) | (bits << (64 - n))) & (2**64 - 1)
+    
+    def encryptData(self, plaintext):
+        with open(plaintext, 'rb') as text_bytes:
+            input_bytes = text_bytes.read()
+        byte_length = len(input_bytes)
+        s_0 = self.initCipher()
+        z_res = self.make_keystream(s_0[0], s_0[1], s_0[2], byte_length)
+        z = bytes.fromhex(z_res)
+        ciphertext = ''
+        for i in range(byte_length):
+            xor = hex(input_bytes[i] ^ z[i])[2:]
+            if len(xor) < 2:
+                xor = '0' + xor
+            ciphertext += xor
+        return ciphertext, z_res
+    
+    def decryptData(self, ciphertext, z):
+        z_i = bytes.fromhex(z)
+        ciphertext_i = bytes.fromhex(ciphertext)
+        res = []
+        for i in range(len(ciphertext_i)):
+            res.append(z_i[i] ^ ciphertext_i[i])
+        res_bytes = bytes(res)
+        return res_bytes.decode('utf-8')
